181a2d2612c221d97c57388aa8912965018cea79
Vande Cavassin <cavassin.vanderlei@gmail.com>
2020-04-23 00:33:22 -0300

    Commit pre/pos configuração inicial

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main(){
+  runApp(MaterialApp(
+    title: "Lançador de Pedido",
+    home: Container(color: Colors.white,)
+  ));
+
+}
