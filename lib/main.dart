4c024fb323c2f9cb6dd006312077e7efcc4400e7
Vande Cavassin <cavassin.vanderlei@gmail.com>
2020-04-28 00:03:58 -0300

    commit pre-criacao listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 void main() {
   runApp(MaterialApp(title: "Lançador de Pedido", home: Home()));
@@ -11,16 +14,71 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Map _Clientes = {};
+
+  Future<Map> _buscarClientes() async {
+    var url = "http://192.168.15.9/CadApi/Clientes/getClientes";
+    http.Response busca = await http.post(url, body: {"idusuario": '1'});
+    _Clientes = json.decode(busca.body);
+    print('Response body: ' + _Clientes["dados"][0].toString());
+    // _Clientes =
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Controles"),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: SingleChildScrollView(
+            child: Container(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                            child: TextField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                              labelText: "Buscar Cliente",
+                              labelStyle: TextStyle(
+                                  color: Colors.blue, fontSize: 14.0)),
+                        )),
+                        RaisedButton(
+                          child: Icon(Icons.search),
+                          onPressed: () {
+                            _buscarClientes();
+                          },
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ListView.builder(
+                            padding: EdgeInsets.all(5),
+                            itemCount: 1,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: _Clientes["dados"][index]["nm_cliente"],
+                              );
+                            })
+                      ],
+                    )
+                  ],
+                ))));
+  }
+
+  @override
+  // TODO: implement widget
+  Widget _ListaCLientes(BuildContext context, int index) {
     return Container(
       color: Colors.white,
-      child: RaisedButton(
-        child: Text("Abrir Modal"),
-        onPressed: () {
-          DialogBusca(context);
-        },
-      ),
     );
   }
 
@@ -34,7 +92,7 @@ class _HomeState extends State<Home> {
             Container(
               padding: EdgeInsets.all(12),
               alignment: Alignment.center,
-              decoration: BoxDecoration(color: Colors.grey[300]),
+              decoration: BoxDecoration(color: Colors.grey[100]),
               child: Text(
                 "Dialog With Image",
                 style: TextStyle(
@@ -47,6 +105,6 @@ class _HomeState extends State<Home> {
         ),
       ),
     );
-    showDialog(context: context, builder: (BuildContext context)=> dialogo);
+    showDialog(context: context, builder: (BuildContext context) => dialogo);
   }
 }
